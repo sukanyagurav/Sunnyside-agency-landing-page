5c8c661145993f1fa3aa3794312b80c801cfc7bf
sukanyagurav <sukanyagurav6@gmail.com>
2023-10-16 17:37:55 +0530

    update hamburger

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const toggleBtn = document.querySelector('.hamburger-menu')
 const menu = document.querySelector('.menu-wrap')
-
+const backdrop =document.querySelector('.backdrop')
 toggleBtn.addEventListener('click',function(){
-    menu.classList.toggle('active')
+    if(menu.classList.contains('active')){
+        toggleBtn.style.opacity='1'
+        menu.classList.remove('active')
+    }else{
+        toggleBtn.style.opacity='0.6'
+        menu.classList.add('active')
+    }
 })
+window.addEventListener('resize', function(event) {
+    if(menu.classList.contains('active')){
+        toggleBtn.style.opacity='1'
+        menu.classList.remove('active')
+    }
+}, true);
