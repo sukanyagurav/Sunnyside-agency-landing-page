051d1a3b360eb804e2c159d50a4bfc6c1110f38b
sukanyagurav <sukanyagurav6@gmail.com>
2023-10-21 23:00:42 +0530

    added animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const toggleBtn = document.querySelector('.hamburger-menu')
 const menu = document.querySelector('.menu-wrap')
-const backdrop =document.querySelector('.backdrop')
-toggleBtn.addEventListener('click',function(){
+toggleBtn.addEventListener('click',function(e){
+    e.stopPropagation()
     if(menu.classList.contains('active')){
         toggleBtn.style.opacity='1'
         menu.classList.remove('active')
@@ -15,4 +15,25 @@ window.addEventListener('resize', function(event) {
         toggleBtn.style.opacity='1'
         menu.classList.remove('active')
     }
-}, true);
+});
+window.addEventListener('click', function(event) {
+    toggleBtn.style.opacity='1'
+    menu.classList.remove('active')
+});
+
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'50px',
+    duration:1300,
+    delay:100,
+    reset:true
+})
+sr.reveal(`.banner`)
+sr.reveal(`.about-text`)
+sr.reveal(`.testimonials-container div:nth-child(1)`,{origin:'bottom',opacity:1})
+sr.reveal(`.testimonials-container div:nth-child(2)`,{origin:'bottom',opacity:1})
+sr.reveal(`.testimonials-container div:nth-child(3)`,{origin:'bottom',opacity:1})
+sr.reveal(`.projects picture:nth-child(1)`,{origin:'bottom',opacity:1,delay:100})
+sr.reveal(`.projects picture:nth-child(2)`,{origin:'bottom',opacity:1,delay:200})
+sr.reveal(`.projects picture:nth-child(3)`,{origin:'bottom',opacity:1,delay:300})
+sr.reveal(`.projects picture:nth-child(4)`,{origin:'bottom',opacity:1,delay:400})
